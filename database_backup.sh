828e6829d940004858eb9d691c403682a7fc26a1
shamkumbhar <shamkumbhar47@gmail.com>
2025-07-03 18:41:10 +0530

    Update database_backup.sh

--- database_backup.sh
+++ database_backup.sh
@@ -31,3 +31,16 @@ echo "[INFO] Removing local backup file $LOCAL_BACKUP_FILE..."
 rm -f "$LOCAL_BACKUP_FILE"
 
 echo "✅ Backup and upload complete!"
+
+
+
+#!/bin/bash
+
+CRON_SCHEDULE="45 18 * * *"  # 5:30 PM IST
+CRON_COMMAND="/bin/bash /home/ubuntu/database_backup/database_backup.sh >> /home/ubuntu/database_backup/cron_backup.log 2>&1"
+
+# Add the cron job
+(crontab -l 2>/dev/null; echo "$CRON_SCHEDULE $CRON_COMMAND") | crontab -
+
+echo "[INFO] Cron job added."
+
